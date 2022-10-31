396c4ee3fc717dd722ae664a448e707d86da1efb
AlanHaman <114674762+AlanHaman@users.noreply.github.com>
2022-10-31 17:04:43 +0100

    Update taylor_sine.c

--- src/taylor_sine.c
+++ src/taylor_sine.c
@@ -4,7 +4,7 @@
 #include <math.h>
 double taylor_sine(double x, int n) {
     /* implement your function here */
-    // assert to make sur n>0
+    // assert to make sure n>0
     assert(n > 0);
 
     
@@ -13,37 +13,33 @@ double taylor_sine(double x, int n) {
     int round = 1;
     int sign = 1;
 
-    // Jeg laver en for løkke så man regner Sinus med n præcision.
+    // for loop to calculate sine with n precision
     for (int i = 1; i <= n; i++) {
-        // Jeg opretter en variabel, som skal indeholde værdien af brøken for
-        // hvert gennemløb.
+        
         double fraction;
 
-        // Dette for loop regner tælleren i brøken. Altså x^runde. Jeg gemmer
-        // svaret i variablen top.
+        // Another for-loop to calculate the numerator in the fraction
         double top = x;
         for (int j = 1; j < round; j++) {
             top = top * x;
         }
 
-        // Her er et for loop som regner nævneren i brøken. Altså fakulteten af
-        // runde. Den gemmer jeg i variablen bottom.
+        // Another for-loop to calculate the denominator in the fraction
         double bottom = 1;
         for (int j = 1; j <= round; j++) {
             bottom = bottom * j;
         }
 
-        // Fraction er brøken altså top delt med bottom.
+       
         fraction = top / bottom;
 
-        // Her opdaterer jeg Sine variablen til indeholde den nye brøk *
-        // fortegnet sign.
+       
         sine = sine + (sign * fraction);
 
-        // Her opdaterer jeg sign, så fortegnet skifter mellem -1 og 1.
+        
         sign = sign * -1;
 
-        // Her opdaterer jeg runde, så runde skifter i intervallet 1, 3, 5 osv..
+        // Here we update the round, so it changes to 1, 3, 5...
         round = round + 2;
     }
 
